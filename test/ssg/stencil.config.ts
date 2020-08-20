ebd5634dbd6f162e8da0db07abbab93ca8e7c6f0
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-08-20 14:36:55 -0500

    wip

--- test/ssg/stencil.config.ts
+++ test/ssg/stencil.config.ts
@@ -3,8 +3,10 @@ import { Config } from '../../internal';
 export const config: Config = {
   namespace: 'SSG',
   outputTargets: [{ type: 'www', baseUrl: 'https://ssg.stenciljs.com/', serviceWorker: false }],
+  maxConcurrentWorkers: 0,
   devServer: {
     logRequests: true,
+    worker: false,
   },
   hashFileNames: false,
 };
