68a56e9b8e5342ee698245e1eb99b05f780e60da
Manu MA <manu.mtza@gmail.com>
2020-09-02 18:09:53 +0200

    Update server-plugin.ts

--- src/compiler/bundle/server-plugin.ts
+++ src/compiler/bundle/server-plugin.ts
@@ -25,86 +25,32 @@ export const serverPlugin = (config: d.Config, platform: string): Plugin => {
       if (id === serverVarid) {
         return id;
       }
-      if (isHydrateBundle && externals.includes(id)) {
-        // don't attempt to bundle node builtins for the hydrate bundle
-        return {
-          id,
-          external: true,
-        };
+      if (isHydrateBundle) {
+        if (externals.includes(id)) {
+          // don't attempt to bundle node builtins for the hydrate bundle
+          return {
+            id,
+            external: true,
+          };
+        }
+        id = normalizeFsPath(id);
+        if (id.includes('.server/') || id.endsWith('.server')) {
+          // any path that has .server in it shouldn't actually
+          // be bundled in the web build, only the hydrate build
+          return serverVarid;
+        }
       }
       return null;
     },
 
     load(id) {
       if (id === serverVarid) {
-        // shared variable we'll use to represent code that was removed
-        return `export const removedServerVar = {/* server-side only code removed from client build */};`;
-      }
-      return null;
-    },
-
-    transform(code, id) {
-      if (/\0/.test(id)) {
-        return null;
-      }
-
-      if (!isHydrateBundle) {
-        id = normalizeFsPath(id);
-        if (id.includes('.server/') || id.endsWith('.server')) {
-          // any path that has .server in it shouldn't actually
-          // be bundled in the web build, only the hydrate build
-          return removeServerCode(id, code);
-        }
+        return {
+          code: 'export default {};',
+          syntheticNamedExports: true,
+        };
       }
       return null;
     },
   };
-};
-
-const removeServerCode = (fileName: string, code: string) => {
-  const output: string[] = [];
-
-  // shared variable we'll use to represent code that was removed
-  output.push(`import { removedServerVar } from '@removed-server-code';`);
-
-  const tsSourceFile = ts.createSourceFile(fileName, code, ts.ScriptTarget.ES2018);
-
-  tsSourceFile.statements.forEach(s => {
-    if (ts.isExportDeclaration(s) && s.exportClause && ts.isNamedExports(s.exportClause) && s.exportClause.elements) {
-      // export { dmc }
-      s.exportClause.elements.forEach(element => {
-        if (element.name && element.name.text) {
-          output.push(`export const ${element.name.text} = removedServerVar;`);
-        }
-      });
-    } else if (ts.isExportAssignment(s)) {
-      // export default
-      output.push(`export default removedServerVar;`);
-    } else if (
-      ts.isVariableStatement(s) &&
-      s.modifiers &&
-      s.modifiers.some(m => m.kind === ts.SyntaxKind.ExportKeyword) &&
-      s.declarationList &&
-      s.declarationList.declarations
-    ) {
-      // export const mph = 88;
-      s.declarationList.declarations.forEach(declaration => {
-        if (declaration.name && ts.isIdentifier(declaration.name)) {
-          output.push(`export const ${declaration.name.text} = removedServerVar;`);
-        }
-      });
-    } else if (
-      (ts.isFunctionDeclaration(s) || ts.isClassDeclaration(s)) &&
-      s.modifiers &&
-      s.modifiers.some(m => m.kind === ts.SyntaxKind.ExportKeyword) &&
-      s.name &&
-      ts.isIdentifier(s.name)
-    ) {
-      // export function doc() {}
-      // export class Marty() {}
-      output.push(`export const ${s.name.text} = removedServerVar;`);
-    }
-  });
-
-  return output.join('\n');
 };
